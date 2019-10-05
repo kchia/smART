42c4396199584a96268d6e04bf3fe010ecaac82c
Jared Morgan <jared@majordanger.com>
2019-10-05 16:17:38 -0500

    index and tutorial layout and styling

--- controllers/tutorials.js
+++ controllers/tutorials.js
@@ -10,6 +10,11 @@ router.get("/", (req, res) => {
     .catch(err => console.error(err));
 });
 
+// CUD page router for new tut
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
 // single tut router (R)
 router.get("/:id", (req, res) => {
   Tutorial.findOne({ _id: req.params.id })
@@ -17,12 +22,40 @@ router.get("/:id", (req, res) => {
     .catch(err => console.error(err));
 });
 
-// CUD page router for tut
+// CUD page router for existing tut
+router.get("/:id/edit", (req, res) => {
+  Tutorial.findOne({ _id: req.params.id }).then(tut => {
+    res.render("new", tut);
+  });
+});
 
 // POST router (C)
+router.post("/", (req, res) => {
+  req.body.supplies = req.body.supplies.split("\n");
+  req.body.optional = req.body.optional.split("\n");
+  req.body.steps = req.body.steps.split("\n-");
+
+  Tutorial.create(req.body).then(tut => {
+    res.redirect("/");
+  });
+});
 
 // PUT router (U)
+router.put("/:id", (req, res) => {
+  req.body.supplies = req.body.supplies.split("\n");
+  req.body.optional = req.body.optional.split("\n");
+  req.body.steps = req.body.steps.split("\n-");
+
+  Tutorial.findOneAndUpdate({ _id: req.params.id }, req.body).then(tut => {
+    res.redirect("/edit/:id");
+  });
+});
 
 // DELETE router (D)
+router.delete("/:id", (req, res) => {
+  Tutorial.findOneAndDelete({ _id: req.params.id }).then(() => {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
