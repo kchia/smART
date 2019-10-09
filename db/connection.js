e971d25096ddd1f04759879de87582c4193bedda
Jared Morgan <jared@majordanger.com>
2019-10-08 21:31:34 -0500

    the bug that will not die for launch

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.set("useUnifiedTopology", true);
-
 let mongoURI = "";
 
 if (process.env.NODE_ENV === "production") {
@@ -11,7 +9,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(instance =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
