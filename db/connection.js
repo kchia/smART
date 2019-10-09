906d0a764c1439d7d49d42cf722e05a45e86d3be
Jared Morgan <jared@majordanger.com>
2019-10-08 21:40:34 -0500

    the bug that will not die for launch

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ mongoose.Promise = Promise;
 
 let mongoURI = "";
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/smart";
